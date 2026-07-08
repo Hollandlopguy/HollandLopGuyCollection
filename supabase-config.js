9d5928fc1bc88255137274443f75cd122f57feb5
Hollandlopguy <Hollandlopguy@users.noreply.github.com>
2026-07-08 18:36:48 -0500

    Configure Supabase connection

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.KOOL_AID_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://getxogtwhmnbmlahtzyl.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdldHhvZ3R3aG1uYm1sYWh0enlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1NDc0MTUsImV4cCI6MjA5OTEyMzQxNX0.5fGFk5ZRPOh30NaWGExJPNa9DzOBsQEt_ZvtEDyUCXQ",
 };
